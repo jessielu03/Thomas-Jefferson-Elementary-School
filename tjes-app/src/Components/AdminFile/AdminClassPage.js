0af30fd85b758e1d1f71ddd719d1dc98a29cb43b
Ananya-Ananda <ananya.ananda201@gmail.com>
2022-05-24 15:20:26 -0400

    more components

---
+++ tjes-app/src/Components/AdminFile/AdminClassPage.js
@@ -0,0 +1,8 @@
+
+function AdminClassPage(){
+
+    // Show teacher for each class ->
+    // allow to edit both the teach and the list
+    // of students for that class
+
+}export default AdminClassPage;
