2e631f98bbb451a29a8f64023e1afa80b28f6f44
jm8gw <82823709+jm8gw@users.noreply.github.com>
2022-05-26 15:12:48 -0400

    Teacher Dashboard Table Added

--- tjes-app/src/Components/StudentObj.js
+++ tjes-app/src/Components/StudentObj.js
@@ -1,11 +1,60 @@
+//import Table from '@mui/material/Table';
+//import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+//import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+//import Title from './Title';
+//import Typography from '@mui/material/Typography';
+
 function StudentObj(props) {
     return (
         <div>
-            <h3>{props.studentFirst} {props.studentLast}</h3>
-            <p>Current Grade: {props.gradeLetter}</p>
-            <input type="submit" onClick={() => props.upvote(props.id)} value="Upvote" />
+        <TableRow key={props.id}>
+            <TableCell>{props.studentFirst}</TableCell>
+            <TableCell>{props.studentLast}</TableCell>
+            <TableCell>{props.studentClass}</TableCell>
+            <TableCell>{props.gradeLetter}</TableCell>
+            <TableCell align="right">
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        //value={age}
+                        label="Age"
+                        //onChange={() => props.upvote(props.id)} value="Upvote"
+                        type="submit"
+                    >
+                        <MenuItem value={4}>A</MenuItem>
+                        <MenuItem value={3.7}>A-</MenuItem>
+                        <MenuItem value={3.3}>B+</MenuItem>
+                        <MenuItem value={3.0}>B</MenuItem>
+                        <MenuItem value={2.7}>B-</MenuItem>
+                        <MenuItem value={2.3}>C+</MenuItem>
+                        <MenuItem value={2.0}>C</MenuItem>
+                        <MenuItem value={1.7}>C-</MenuItem>
+                        <MenuItem value={1.3}>D+</MenuItem>
+                        <MenuItem value={1.0}>D</MenuItem>
+                        <MenuItem value={0.7}>D-</MenuItem>
+                        <MenuItem value={0.0}>F</MenuItem>
+                    </Select>
+                </FormControl>
+            </TableCell>
+        </TableRow>
         </div>
     );
 }
 
 export default StudentObj;
+
+{/*
+<div>
+<h3>{props.studentFirst} {props.studentLast}</h3>
+<p>Current Grade: {props.gradeLetter}</p>
+<input type="submit" onClick={() => props.upvote(props.id)} value="Upvote" />
+</div>
+*/}
