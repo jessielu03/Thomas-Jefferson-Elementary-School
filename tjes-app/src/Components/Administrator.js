9f8f4b74f46a31400671e59c4078a3047d5987c3
jessielu03 <jessielu03@gmail.com>
2022-05-26 09:22:36 -0400

    added admin dashboard functions

--- tjes-app/src/Components/Administrator.js
+++ tjes-app/src/Components/Administrator.js
@@ -3,35 +3,63 @@ import React from "react";
 import db from '../database.js';
 import {useEffect} from "react";
 import { useState } from "react";
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import SchoolIcon from '@mui/icons-material/School';
 import { getFirestore, collection, addDoc, doc, getDocs, updateDoc, increment } from "firebase/firestore";
 
 
-function Administrator(){
+function Administrator() {
 
-    const [responses, setResponses] = useState([])
+  const [classList, setClassList] = useState([]);
 
+  useEffect(() => {
+    console.log("rerendering...");
+      const classes = []
+      getDocs(collection(db, "Classes"))
+      .then((allClasses) => {
+        allClasses.forEach((c) => classes.push({ id: c.id, ...c.data() }))
+        classes.sort()  // TODO
+        setClassList(classes)
+      })
+  }, [db])
 
-    useEffect(() => {
-        const classes = []
-        getDocs(collection(db, "Classes"))
-        .then((allClasses) => {
-          allClasses.forEach((c) => classes.push({ id: c.id, ...c.data() }))
-          classes.sort()  // TODO
-          setResponses(responses)
-        })
-      }, [db])
+  return(
+      <nav>
+          <Link to='/'>Home</Link>
+          <h1>Welcome, admin</h1>
 
-    return(
-        <nav>
-            <Link to='/'>Home</Link>
-            <h1>Welcome, admin</h1>
-            <Link to='administrator/TeacherDirectory'>Teacher Directory</Link>
-            <Link to='administrator/StudentDirectory'>Student Directory</Link>
+          
+          <Link to='TeacherDirectory'><Button
+              variant='outlined'
+              sx={{ color: 'purple', borderColor: 'purple' }}>
+                <SchoolIcon />Teacher Directory</Button></Link>
+          
+          <Link to='StudentDirectory'><Button
+              variant='outlined'
+              sx={{ color: 'purple', borderColor: 'purple' }}>
+                <SchoolIcon />Student Directory</Button></Link>
+          <br></br>
+          <br></br>
+          <Divider></Divider>
 
-            {/* links to
-                    -use mapping to create links to each
-                    class page component
-            */}
-        </nav>
-    );
-}export default Administrator;
+          {/*temporary link to access class page*/}
+          <h3>Class Pages:</h3>
+          <Link to='AdminClassPage' state={{ className: "1A", classID: "tempID"}}>
+            <Button
+              variant='outlined'
+              sx={{ color: 'purple', borderColor: 'purple' }}>Class 1A</Button>
+          </Link>
+
+          <br></br>
+        
+          {classList.map((c) => 
+            <Link to='AdminClassPage' state={{ className: c.name, classID: c.id }}>
+              <button>Class {c.name}</button>
+            </Link>)
+          }
+      </nav>
+  );
+}
+
+export default Administrator;
